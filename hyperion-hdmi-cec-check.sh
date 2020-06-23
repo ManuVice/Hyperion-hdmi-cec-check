8680bb8bd62d27e189206cb4c033e0481ee80f6a
ManuVice <el.manu87@gmail.com>
2020-06-23 19:53:18 +0200

    Update hyperion-hdmi-cec-check.sh

--- hyperion-hdmi-cec-check.sh
+++ hyperion-hdmi-cec-check.sh
@@ -1,54 +1,34 @@
-#!/bin/sh
+#!/bin/bash
 
-# Copyright 2016 by Spegeli from PowerPi.de Forum (http://powerpi.de/forum/viewtopic.php?p=11712#p11712)
-# Modifed 2018 by ManuVice
-# Tested with Hyperion.ng #e438bc6 (https://github.com/hyperion-project/hyperion.ng) and Raspbian Stretch
+# Script for hyperion which allows you to turn off your LEDs when your AVR is off and vice versa.
+# Tested with Raspian on an RPi3
+# 1 - Plug your RPi with an HDMI cable TV/AVR/...
+# 2 - Copy the script on your RPi
+# 3 - Run sudo chmod +x on the script
+# 4 - To make it run at startup, add   @reboot /your/path/yourscriptname.sh    to crontab -e
+# 5 - Enjoy
 
-# Please enter time in seconds
-CheckTime=30 #How often should it check if TV and/or Hyperion is running? Standard 15 (Personally I've set it to 60)
+# created by RaPiiDe
+# modified by ManuVice
 
-#Do not change!
-path=$@ #Get the Path of the Config File
+remotePath="PathToYourHyperion-remote"
+isOn=-1
 
-while :; do
-    echo "ask CEC status"
-    command="echo pow 0 | cec-client -s -d 1 | grep 'power status'"
-    status=$(eval $command)
-    echo $status
-
-    if [ "$status" = "power status: standby" ] || [ "$status" = "power status: unknown" ]; then
-      echo "TV is off at the moment!"
-      #Ask Hyperion if it's already running
-          if $(systemctl -q is-active hyperion.service)
-      then
-         echo "Hyperion is running - stop it!"
-         sleep 15 #give Hyperion some time to disable lights
-         eval sudo systemctl stop hyperion.service
-         sleep 1
-         killall hyperiond
-         sleep $CheckTime
-      else
-         echo "Hyperion isn't running!"
-      fi
-    elif [ "$status" = "power status: on" ] || [ "$status" = "power status: in transition from standby to on" ]; then
-      echo "TV is on at the moment!"
-      #Ask Hyperion if it's already running
-          if $(systemctl -q is-active hyperion.service)
-      then
-         echo "Hyperion is running!"
-      else
-         echo "Hyperion isn't running - start it!"
-         killall hyperiond
-         sleep 5 #wait some time get an image. <-- You can change or comment this out if you dont want it!!!
-         eval sudo systemctl start hyperion.service
-         sleep 1
-      fi
-    fi
-
-    echo "Wait $CheckTime sec before next check"
-    sleep $CheckTime
-
-echo $status
-
+while :
+do
+        # We get the CEC status
+        status=$(echo pow 0 | timeout 5 cec-client -d 1 -s)
+        # If it's off and it wasn't already, let's turn off the LEDS
+        if [[ "$status" == *"power status: standby"* ]] && [[ "$isOn" != "0" ]]
+        then
+                eval "$remotePath --off"
+                isOn=0
+        # If it's on and it wasn't already, let's turn on the LEDS
+        elif [[ "$status" == *"power status: on"* ]] && [[ "$isOn" != "1" ]]
+        then
+                eval "$remotePath --on"
+                isOn=1
+        fi
+		
+        sleep 10 
 done
-exit 0
